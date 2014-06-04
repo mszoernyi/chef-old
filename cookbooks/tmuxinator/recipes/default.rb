18f3baaa41d130d67f6c3776d96764275eece012
Mike Szörnyi <mike.szoernyi@gmail.com>
2014-06-04 14:21:18 +0000

    add tmuxinator projects: chef, company-site

--- cookbooks/tmuxinator/recipes/default.rb
+++ cookbooks/tmuxinator/recipes/default.rb
@@ -1,6 +1,16 @@
 gem_package "tmuxinator"
 
 if !root?
+  overridable_template "#{node[:homedir]}/.tmuxinator/chef.yml" do
+    source "tmuxinator/chef.yml"
+    cookbook "users"
+    instance node[:current_user]
+  end
+  overridable_template "#{node[:homedir]}/.tmuxinator/company-site.yml" do
+    source "tmuxinator/company-site.yml"
+    cookbook "users"
+    instance node[:current_user]
+  end
   overridable_template "#{node[:homedir]}/.tmuxinator/dashboard.yml" do
     source "tmuxinator/dashboard.yml"
     cookbook "users"
