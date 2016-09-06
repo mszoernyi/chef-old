52e21782709a491becdee9616fa4cb5edbcd6264
Mike Szörnyi <mike.szoernyi@gmail.com>
2016-09-07 01:37:22 +0200

    simplify tmuxinator configuration
    All project are stored at ~/projects/<own|remerge>/ and tmuxinator
    offers two settings now.
    
    `mux start remerge ember-remerge` will open a new session with 3 windows
    and will set the working dir to ~/projects/remerge/ember-remerge

--- cookbooks/tmuxinator/recipes/default.rb
+++ cookbooks/tmuxinator/recipes/default.rb
@@ -1,48 +1,20 @@
-gem_package "tmuxinator"
+gem_package "tmuxinator" do
+  action :upgrade
+end
+
+template "#{node[:script_path]}/tmuxinator-autocomplete.sh" do
+  source "autocomplete.sh"
+  mode "0755"
+end
 
 if !root?
-  overridable_template "#{node[:homedir]}/.tmuxinator/chef.yml" do
-    source "tmuxinator/chef.yml"
+  overridable_template "#{node[:homedir]}/.tmuxinator/remerge.yml" do
+    source "tmuxinator/remerge.yml"
     cookbook "users"
     instance node[:current_user]
   end
-  overridable_template "#{node[:homedir]}/.tmuxinator/company-site.yml" do
-    source "tmuxinator/company-site.yml"
-    cookbook "users"
-    instance node[:current_user]
-  end
-  overridable_template "#{node[:homedir]}/.tmuxinator/dashboard.yml" do
-    source "tmuxinator/dashboard.yml"
-    cookbook "users"
-    instance node[:current_user]
-  end
-  overridable_template "#{node[:homedir]}/.tmuxinator/ember-remerge.yml" do
-    source "tmuxinator/ember-remerge.yml"
-    cookbook "users"
-    instance node[:current_user]
-  end
-  overridable_template "#{node[:homedir]}/.tmuxinator/remerge-admin.yml" do
-    source "tmuxinator/remerge-admin.yml"
-    cookbook "users"
-    instance node[:current_user]
-  end
-  overridable_template "#{node[:homedir]}/.tmuxinator/remerge-my.yml" do
-    source "tmuxinator/remerge-my.yml"
-    cookbook "users"
-    instance node[:current_user]
-  end
-  overridable_template "#{node[:homedir]}/.tmuxinator/remerge-ui.yml" do
-    source "tmuxinator/remerge-ui.yml"
-    cookbook "users"
-    instance node[:current_user]
-  end
-  overridable_template "#{node[:homedir]}/.tmuxinator/remerge-api.yml" do
-    source "tmuxinator/remerge-api.yml"
-    cookbook "users"
-    instance node[:current_user]
-  end
-  overridable_template "#{node[:homedir]}/.tmuxinator/remerge-backend.yml" do
-    source "tmuxinator/remerge-backend.yml"
+  overridable_template "#{node[:homedir]}/.tmuxinator/own.yml" do
+    source "tmuxinator/own.yml"
     cookbook "users"
     instance node[:current_user]
   end
